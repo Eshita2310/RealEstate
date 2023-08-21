fbe03570bd99c85a1a33ed8439d08f945d14e85a
Eshita Upadhyaya <76433870+Eshita2310@users.noreply.github.com>
2023-08-21 21:43:01 +0530

    Add files via upload

---
+++ home_db.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2023 at 07:16 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `home_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `password`) VALUES
+('BcjKNX58e4x7bIqIvxG7', 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2'),
+('RkTmAoba01yCaZv0jg2M', 'admin1', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
+
+CREATE TABLE `property` (
+  `id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `property_name` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `price` varchar(10) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `offer` varchar(10) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `furnished` varchar(50) NOT NULL,
+  `bhk` varchar(10) NOT NULL,
+  `deposite` varchar(10) NOT NULL,
+  `bedroom` varchar(10) NOT NULL,
+  `bathroom` varchar(10) NOT NULL,
+  `balcony` varchar(10) NOT NULL,
+  `carpet` varchar(10) NOT NULL,
+  `age` varchar(2) NOT NULL,
+  `total_floors` varchar(2) NOT NULL,
+  `room_floor` varchar(2) NOT NULL,
+  `loan` varchar(50) NOT NULL,
+  `lift` varchar(3) NOT NULL DEFAULT 'no',
+  `security_guard` varchar(3) NOT NULL DEFAULT 'no',
+  `play_ground` varchar(3) NOT NULL DEFAULT 'no',
+  `garden` varchar(3) NOT NULL DEFAULT 'no',
+  `water_supply` varchar(3) NOT NULL DEFAULT 'no',
+  `power_backup` varchar(3) NOT NULL DEFAULT 'no',
+  `parking_area` varchar(3) NOT NULL DEFAULT 'no',
+  `gym` varchar(3) NOT NULL DEFAULT 'no',
+  `shopping_mall` varchar(3) NOT NULL DEFAULT 'no',
+  `hospital` varchar(3) NOT NULL DEFAULT 'no',
+  `school` varchar(3) NOT NULL DEFAULT 'no',
+  `market_area` varchar(3) NOT NULL DEFAULT 'no',
+  `image_01` varchar(50) NOT NULL,
+  `image_02` varchar(50) NOT NULL,
+  `image_03` varchar(50) NOT NULL,
+  `image_04` varchar(50) NOT NULL,
+  `image_05` varchar(50) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `property`
+--
+
+INSERT INTO `property` (`id`, `user_id`, `property_name`, `address`, `price`, `type`, `offer`, `status`, `furnished`, `bhk`, `deposite`, `bedroom`, `bathroom`, `balcony`, `carpet`, `age`, `total_floors`, `room_floor`, `loan`, `lift`, `security_guard`, `play_ground`, `garden`, `water_supply`, `power_backup`, `parking_area`, `gym`, `shopping_mall`, `hospital`, `school`, `market_area`, `image_01`, `image_02`, `image_03`, `image_04`, `image_05`, `description`, `date`) VALUES
+('ujZ9PPxB2Wqlxt165EIP', 'sB7ncLgfOQLK55aO3Lzq', 'FALCON', 'sus,Pune', '50000', 'flat', 'rent', 'ready to move', 'furnished', '3', '150000', '3', '3', '2', '1600', '5', '20', '10', 'not available', 'yes', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', 'no', 'no', 'no', 'yes', 'nx6B4NBIe0z9okXqjEgS.png', 'z0xv7gDKc9WSW8rJHcOw.png', '8Fq1CtQguJYhqSkSI87v.png', 'UKNx8kFqwpOkEEzgA72B.png', 'QCWi8joxrOYgKw113txb.png', 'Very Beautiful and lavish flat with thousands of amenities. Fully furnished and ready to move in anytime you want.', '2023-04-14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `id` varchar(20) NOT NULL,
+  `property_id` varchar(20) NOT NULL,
+  `sender` varchar(20) NOT NULL,
+  `receiver` varchar(20) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saved`
+--
+
+CREATE TABLE `saved` (
+  `id` varchar(20) NOT NULL,
+  `property_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `saved`
+--
+
+INSERT INTO `saved` (`id`, `property_id`, `user_id`) VALUES
+('5puOhPe8oRhK2HrjfPJf', 'ujZ9PPxB2Wqlxt165EIP', 'V5XK7ZE0OoCY4lfAsV9l');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `number`, `email`, `password`) VALUES
+('sB7ncLgfOQLK55aO3Lzq', 'USER1', '9876543210', 'user1@gmail.com', 'a8296f4e3bcbc90445bc7e974bede37b4af6aebf'),
+('V5XK7ZE0OoCY4lfAsV9l', 'unnati jha', '9876543210', 'unnati@gmail.com', 'a8296f4e3bcbc90445bc7e974bede37b4af6aebf');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
